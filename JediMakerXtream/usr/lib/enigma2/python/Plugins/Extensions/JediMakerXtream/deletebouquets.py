711249cb3e810eafe2e3bb5ee957c34ecb898ccc
kiddac <kiddac2015@gmail.com>
2020-09-09 19:36:03 +0100

    pep8 cleanup

--- JediMakerXtream/usr/lib/enigma2/python/Plugins/Extensions/JediMakerXtream/deletebouquets.py
+++ JediMakerXtream/usr/lib/enigma2/python/Plugins/Extensions/JediMakerXtream/deletebouquets.py
@@ -47,22 +47,20 @@ class JediMakerXtream_DeleteBouquets(Screen):
         self.onLayoutFinish.append(self.__layoutFinished)
 
         self['setupActions'] = ActionMap(['SetupActions', 'ColorActions'], {
-         'red': self.keyCancel,
-         'green': self.deleteBouquets,
-         'yellow': self.toggleAllSelection,
-         'blue': self.clearAllSelection,
-         'save': self.deleteBouquets,
-         'cancel': self.keyCancel,
-         'ok': self.toggleSelection
-         }, -2)
+            'red': self.keyCancel,
+            'green': self.deleteBouquets,
+            'yellow': self.toggleAllSelection,
+            'blue': self.clearAllSelection,
+            'save': self.deleteBouquets,
+            'cancel': self.keyCancel,
+            'ok': self.toggleSelection
+        }, -2)
 
         self.getStartList()
         self.refresh()
 
-
     def __layoutFinished(self):
         self.setTitle(self.setup_title)
-
 
     def buildListEntry(self, name, index, enabled):
         if enabled:
@@ -71,7 +69,6 @@ class JediMakerXtream_DeleteBouquets(Screen):
             pixmap = LoadPixmap(cached=True, path=skin_path + "images/lock_off.png")
         return(pixmap, str(name), index, enabled)
 
-
     def getStartList(self):
         for playlist in self.playlists_all:
             if 'bouquet_info' in playlist and 'oldname' in playlist['bouquet_info']:
@@ -79,12 +76,10 @@ class JediMakerXtream_DeleteBouquets(Screen):
 
         self.drawList = [self.buildListEntry(x[0], x[1], x[2]) for x in self.startList]
 
-
     def refresh(self):
         self.drawList = []
         self.drawList = [self.buildListEntry(x[0], x[1], x[2]) for x in self.startList]
         self['list'].updateList(self.drawList)
-
 
     def toggleSelection(self):
         if len(self['list'].list) > 0:
@@ -100,16 +95,13 @@ class JediMakerXtream_DeleteBouquets(Screen):
     def getSelectionsList(self):
         return [item[0] for item in self.startList if item[2]]
 
-
     def clearAllSelection(self):
         for idx, item in enumerate(self['list'].list):
             self.startList[idx][2] = False
         self.refresh()
 
-
     def keyCancel(self):
         self.close()
-
 
     def deleteBouquets(self):
         selectedBouquetList = self.getSelectionsList()
@@ -145,7 +137,6 @@ class JediMakerXtream_DeleteBouquets(Screen):
             jglob.current_selection = 0
             jglob.current_playlist = []
         self.close()
-
 
     def deleteBouquetFile(self, bouquet_name):
         for playlist in self.playlists_all:
